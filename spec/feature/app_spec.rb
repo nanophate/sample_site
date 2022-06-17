a8c2ba1fc1eb4379b56f8847f845ea086182ca9c
Naoya Matayoshi <nanophate@users.noreply.github.com>
2022-06-17 12:28:36 +0900

    add some update

--- spec/feature/app_spec.rb
+++ spec/feature/app_spec.rb
@@ -8,5 +8,37 @@ describe 'app', type: :feature, js: true do
     click_on 'Greeting!'
     expect(page).not_to have_content('Hello, Thank you for accessing to this website.')
     expect(page).to have_content('Hi, Thank you for touch me!')
+    puts page.driver.browser
+    sleep 5
+  end
+
+  it 'clicks greeting to change welcome message' do
+    visit '/'
+    expect(page).to have_content('Hello, Thank you for accessing to this website.')
+    expect(page).not_to have_content('Hi, Thank you for touch me!')
+    click_on 'Greeting!'
+    expect(page).not_to have_content('Hello, Thank you for accessing to this website.')
+    expect(page).to have_content('Hi, Thank you for touch me!')
+    sleep 5
+  end
+
+  it 'clicks greeting to change welcome message' do
+    visit '/'
+    expect(page).to have_content('Hello, Thank you for accessing to this website.')
+    expect(page).not_to have_content('Hi, Thank you for touch me!')
+    click_on 'Greeting!'
+    expect(page).not_to have_content('Hello, Thank you for accessing to this website.')
+    expect(page).to have_content('Hi, Thank you for touch me!')
+    sleep 5
+  end
+
+  it 'clicks greeting to change welcome message' do
+    visit '/'
+    expect(page).to have_content('Hello, Thank you for accessing to this website.')
+    expect(page).not_to have_content('Hi, Thank you for touch me!')
+    click_on 'Greeting!'
+    expect(page).not_to have_content('Hello, Thank you for accessing to this website.')
+    expect(page).to have_content('Hi, Thank you for touch me!')
+    sleep 5
   end
 end
